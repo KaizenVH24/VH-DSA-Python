b16514a47df986e02526033ef91bab3ec1dd693b
Vinay Hulsurkar <186893860+KaizenVH24@users.noreply.github.com>
2026-03-01 10:40:22 +0530

    SQL

--- 01_March.sql
+++ 01_March.sql
@@ -6,3 +6,13 @@ select name from Customer where referee_id != 2 or referee_id is null;
 
 -- 595. Big Countries
 select name, population, area from World where population >= 25000000 or area >= 3000000;
+
+-- 1148. Article Views I
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE viewer_id >= 1
+AND author_id = viewer_id
+ORDER BY author_id
+
+-- 1683. Invalid Tweets
+select tweet_id from Tweets where length(content) > 15;
